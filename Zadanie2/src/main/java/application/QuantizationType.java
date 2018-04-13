1fdb808960660e78e9c2f72af2ce372144599d6d
stachura-203996 <203996@edu.p.lodz.pl>
2018-04-13 18:37:41 +0200

    Adding tabs in GUI

---
+++ Zadanie2/src/main/java/application/QuantizationType.java
@@ -0,0 +1,10 @@
+package application;
+
+public enum QuantizationType {
+
+    uniWithCut("Kwantyzacja równomierna z obcięciem"),
+    uniWithRound("Kwantyzacja równomierna z zaokr¡glaniem");
+
+    QuantizationType(String s) {
+    }
+}
